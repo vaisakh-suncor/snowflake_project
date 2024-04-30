a5b35f5deb3d8c3d19aeb4638750c5a77405f5b3
vaisakh-suncor <vaip@suncor.com>
2024-04-30 16:23:31 +0530

    Update Table Script

--- Table/DimWell.sql
+++ Table/DimWell.sql
@@ -1,4 +1,4 @@
-create or replace TABLE DIMWELL (
+create or replace TABLE edw.DIMWELL (
 	WELLSID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 order COMMENT 'identity',
 	WELLGUID VARCHAR(16777216) NOT NULL,
 	WELLPADROWID VARCHAR(16777216),
